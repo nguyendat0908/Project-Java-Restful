736802bc2e8699c5ea9bd23567021dfabe201d83
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-01-09 00:18:47 +0700

    Update API Login

--- src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.domain.DTO.LoginDTO;
 import vn.hoidanit.jobhunter.domain.DTO.ResLoginDTO;
+import vn.hoidanit.jobhunter.service.UserService;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @RestController
@@ -21,10 +23,13 @@ public class AuthController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserService userService;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil,
+            UserService userService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -42,6 +47,12 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         ResLoginDTO res = new ResLoginDTO();
+        User currentUserDB = this.userService.handleGetUserByUsername(loginDTO.getUsername());
+        if (currentUserDB != null) {
+            ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(currentUserDB.getId(), currentUserDB.getName(),
+                    currentUserDB.getEmail());
+            res.setUser(userLogin);
+        }
         res.setAccessToken(access_token);
 
         return ResponseEntity.ok().body(res);
--- src/main/java/vn/hoidanit/jobhunter/domain/DTO/ResLoginDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/DTO/ResLoginDTO.java
@@ -1,15 +1,25 @@
 package vn.hoidanit.jobhunter.domain.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ResLoginDTO {
     
     private String accessToken;
+    private UserLogin user;
 
-    public String getAccessToken() {
-        return accessToken;
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    // Inner class
+    public static class UserLogin {
+        private long id;
+        private String name;
+        private String email;
     }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
 }
