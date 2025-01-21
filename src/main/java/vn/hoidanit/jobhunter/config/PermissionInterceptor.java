c0b75303f30a22f32185012ff5c544be20b2d9ec
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-01-21 23:10:20 +0700

    Authorization by backend

---
+++ src/main/java/vn/hoidanit/jobhunter/config/PermissionInterceptor.java
@@ -0,0 +1,78 @@
+package vn.hoidanit.jobhunter.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vn.hoidanit.jobhunter.domain.Permission;
+import vn.hoidanit.jobhunter.domain.Role;
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
+import vn.hoidanit.jobhunter.util.error.IdInvalidException;
+
+/**
+ * PermissionInterceptor is an implementation of the HandlerInterceptor
+ * interface
+ * that intercepts HTTP requests and logs request details such as the matching
+ * path,
+ * HTTP method, and request URI.
+ * 
+ * This interceptor is typically used to perform pre-processing before the
+ * request
+ * reaches the controller. In this case, it logs the request details for
+ * debugging
+ * or monitoring purposes.
+ * 
+ * @author Your Name
+ */
+
+public class PermissionInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    @Transactional
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response, Object handler)
+            throws Exception {
+
+        String path = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        System.out.println(">>> RUN preHandle");
+        System.out.println(">>> path= " + path);
+        System.out.println(">>> httpMethod= " + httpMethod);
+        System.out.println(">>> requestURI= " + requestURI);
+
+        // Check permission
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() == true ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        if (email != null && email.isEmpty()) {
+            User user = this.userService.handleGetUserByUsername(email);
+            if (user != null) {
+                Role role = user.getRole();
+                if (role != null) {
+                    List<Permission> permissions = role.getPermissions();
+                    boolean isAllow = permissions.stream()
+                            .anyMatch(item -> item.getApiPath().equals(path) && item.getMethod().equals(httpMethod));
+
+                    if (isAllow == false) {
+                        throw new IdInvalidException("Bạn không có quyền truy cập endpoint này!");
+                    }
+                } else {
+                    throw new IdInvalidException("Bạn không có quyền truy cập endpoint này!");
+                }
+            }
+        }
+
+        return true;
+    }
+}
